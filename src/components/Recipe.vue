9f2e47b20e4e1c0aae1cb6f90f52c7fe08dcdc75
John Matula <john@johnmatu.la>
2020-04-26 14:31:16 -0400

    Add initial views and components

---
+++ src/components/Recipe.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="recipe">
+    <h1>{{ recipe.title }}?</h1>
+    <p>{{ recipe.id }}</p>
+    <p>{{ recipe.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      recipe: {}
+    }
+  },
+  created() {
+    this.recipe = this.updateRecipeOrRedirect(this.$route.params.id)
+  },
+  watch: {
+    $route() {
+      this.recipe = this.updateRecipeOrRedirect(this.$route.params.id)
+    }
+  },
+  methods: {
+    updateRecipeOrRedirect(id) {
+      return this.$store.getters.getRecipeById(id) || this.$router.push("/")
+    }
+  },
+  name: 'Recipe',
+}
+</script>
---
+++ src/views/ListMain.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    ListMain.vue
+  </div>
+</template>
