7d4e4c0e18e80a8ac3922f846a691ccad5976d99
John Matula <john@johnmatu.la>
2020-04-26 14:33:46 -0400

    Add mutations necessary to build recipe store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const recipes = require.context("@/store/recipes",true,/.*\.json/i)
+
+console.log(recipes.keys())
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state: () => ({
+    recipes: {}
+  }),
+  getters: {
+    getRecipeById: (state) => (id) => {
+      return state.recipes[id] || null
+    }
   },
   mutations: {
+    ADD_RECIPE(state, recipe) {
+      if(state.recipes[recipe.id]) {
+        console.warn("Recipe “" + recipe.id + "” already exists.");
+      }
+      state.recipes[recipe.id] = recipe
+    }
   },
   actions: {
+    addRecipes({ commit }) {
+      for (var i = 0; i < recipes.keys().length; ++i) {
+        commit("ADD_RECIPE", require("./recipes/" + recipes.keys()[i].replace(/^\.\//,"")))
+      }
+    }
   },
   modules: {
   }
